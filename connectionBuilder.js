a3f075cf8f15a06b253bca1f57a15184a1eff24d
Shahar Shalev <shahar@firebolt.io>
2020-09-30 10:26:07 +0300

    add all connector files

---
+++ connectionBuilder.js
@@ -0,0 +1,5 @@
+(function dsbuilder(attr) {
+    var urlBuilder = "jdbc:firebolt://" + attr[connectionHelper.attributeServer] +  "/" + attr[connectionHelper.attributeDatabase] + "?";
+
+    return [urlBuilder];
+})
