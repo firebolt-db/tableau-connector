942c77048532a2db82bdea8a90396e0698465280
Petro Tiurin <petro.tiurin@firebolt.io>
2022-04-04 17:05:35 +0000

    Merged in remove-jdbc-params (pull request #9)
    feat: Removing jdbc parameter string
    
    * feat: Removing jdbc parameter string

--- connectionBuilder.js
+++ connectionBuilder.js
@@ -1,10 +1,6 @@
 (function dsbuilder(attr) {
     var urlBuilder = "jdbc:firebolt://" + attr[connectionHelper.attributeServer] +  "/" + attr[connectionHelper.attributeDatabase] + "?";
     var engine = attr[connectionHelper.attributeVendor1];
-    var params = attr[connectionHelper.attributeVendor2];
     if(engine!="") urlBuilder+="engine=" + attr[connectionHelper.attributeVendor1]+"&";
-    params = params.split(' ').join('');
-    params = params.split(',').join('&');
-    urlBuilder += params;
     return [urlBuilder];
 })
