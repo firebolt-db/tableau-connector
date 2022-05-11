6fc2ee3912358ebde6ac571019dddc8e93bc6199
Petro Tiurin <petro.tiurin@firebolt.io>
2022-05-11 11:34:33 +0000

    Merged in add-account-attr (pull request #11)
    Add account parameter

--- connectionProperties.js
+++ connectionProperties.js
@@ -3,6 +3,10 @@
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
     props["engine"] = attr[connectionHelper.attributeVendor1];
+    if (attr[connectionHelper.attributeVendor2]) {
+        // Firebolt account is always lower case
+        props["account"] = attr[connectionHelper.attributeVendor2].toLowerCase();
+    }
 
     return props;
 })
